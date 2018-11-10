51152d0948cd3ce05447d8d88d887cb24c73eea9
jcrogoff <jcrogoff@gmail.com>
2018-11-10 11:32:15 -0500

    updating toolbar

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,28 +1,25 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+import Toolbar from './components/Toolbar/Toolbar';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          <Toolbar />
+          <img src={require('./BUlogo.png')} alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            BUILDING UP CAREERS
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
         </header>
       </div>
     );
   }
 }
 
+
 export default App;
+
+//https://www.youtube.com/watch?v=l6nmysZKHFU
---
+++ my-app/src/components/Toolbar/Toolbar.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const toolbar = props => (
+    <header> 
+        <nav>
+            <div><a href = "/">THE LOGO</a></div>
+            <div>
+                <ul>
+                    <li><a href = ""></a>List1</li>
+                    <li><a href = ""></a>List2</li>
+                </ul>
+            </div>
+        </nav>
+    </header>
+);
+
+export default toolbar;
